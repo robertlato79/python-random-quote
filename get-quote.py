bdfe79c8f926655f1015083f7062ffc8c5a41d95
robertlato79 <robertlato@gmail.com>
2021-04-02 19:55:43 +0200

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,13 @@
-def primary():
+import random #To generate random numbers, we'll use a Python module, a built-in extension of the language.
+
+def main():
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  print(quotes[-1])
+  last = len(quotes) - 1 #variable holds the highest index for the array. 
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
-  primary()
+  main()
