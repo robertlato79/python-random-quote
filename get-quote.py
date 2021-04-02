deb485f6fd86bc876f3fc8656077cb1903c0a643
robertlato79 <robertlato@gmail.com>
2021-04-02 19:34:34 +0200

    Keep it logically awesome

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,9 @@
 def primary():
-   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes[-1])
 
 if __name__== "__main__":
   primary()
